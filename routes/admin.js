3d435e4efd9b718102cc961846a98ab146cd540c
Mr-Shah63 <bsit4thse@gmail.com>
2025-10-03 18:15:49 +0500

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const Course = require("../models/Course.js");
 const Subject = require("../models/Subject");
 
 function isAuthenticated(req, res, next) {
-  if (req.session && req.session.user == process.env.ADMIN_USERNAME) {
+  if (req.session && req.session.user === process.env.ADMIN_USERNAME) {
     return next();
   }
   res.redirect("/admin/login");
@@ -39,31 +39,31 @@ router.get("/dashboard", isAuthenticated, async (req, res) => {
 });
 
 // Add Course (GET form)
-router.get("/course/new", (req, res) => {
+router.get("/course/new", isAuthenticated, (req, res) => {
   res.render("admin/addCourse");
 });
 
 // Add Course (POST)
-router.post("/course/new", async (req, res) => {
+router.post("/course/new", isAuthenticated, async (req, res) => {
   const { code, name, description } = req.body;
   await Course.create({ code, name, description, pageUrl: `/programs/${code}` });
   res.redirect("/admin/dashboard");
 });
 
 // Add Subject (GET form)
-router.get("/subject/new",  async (req, res) => {
+router.get("/subject/new", isAuthenticated, async (req, res) => {
   const courses = await Course.find();
   res.render("admin/addSubject", { courses });
 });
 
 // Add Subject (POST)
-router.post("/subject/new", async (req, res) => {
+router.post("/subject/new", isAuthenticated, async (req, res) => {
   const { courseCode, semester, subjectCode, subjectName, folderLink } = req.body;
   await Subject.create({ courseCode, semester, subjectCode, subjectName, folderLink });
   res.redirect("/admin/dashboard");
 });
 // Delete Course
-router.post("/course/:id/delete",  async (req, res) => {
+router.post("/course/:id/delete", isAuthenticated, async (req, res) => {
   try {
     // Find course first
     const course = await Course.findById(req.params.id);
@@ -86,7 +86,7 @@ router.post("/course/:id/delete",  async (req, res) => {
 
 
 // Delete Subject
-router.post("/subject/:id/delete", async (req, res) => {
+router.post("/subject/:id/delete", isAuthenticated, async (req, res) => {
   try {
     await Subject.findByIdAndDelete(req.params.id);
     res.redirect("/admin/dashboard");
